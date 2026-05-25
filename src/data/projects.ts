e08ba819cc93f6636c3e317baa3a13fb53fedf42
Jonathan <jonathanlambert247@gmail.com>
2026-05-25 13:10:12 -0700

    edit strivvy project link

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -10,7 +10,7 @@ export const projects: Project[] = [
     name: "Strivvy",
     description:
       "Social link sharing iOS app built with React Native and Django.",
-    href: "https://github.com/jonathanlambertt/strivvy-frontend/blob/main/README.md",
+    href: "https://github.com/jonathanlambertt/strivvy-frontend/tree/main",
     image: "/images/strivvy_project.png",
   },
   {
